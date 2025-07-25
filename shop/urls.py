c9e5105ceff1b5f95922a5a17dcc5a1956442e51
Satyam Dubey <dubeysatyam3635@gmail.com>
2025-07-25 15:36:34 +0530

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.index, name="ShopHome"),
+path('', lambda request: redirect('shop/')),
+path('shop/', include('shop.urls')),
 path('about/', views.about, name="AboutUs"),
 path('contact/', views.contact_view, name="contact Us"),
 path('tracker/', views.tracker, name="trackerForm"),
