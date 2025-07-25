46675d70ecedbbbbad4248e5bef9380883fb35c8
Satyam Dubey <dubeysatyam3635@gmail.com>
2025-07-25 14:24:34 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -30,7 +30,7 @@ RAZORPAY_KEY_SECRET = "xxxxxxxxxxxxxxxxxx"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
